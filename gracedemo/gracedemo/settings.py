6c4cdc9729b8ddc3c64d15f9f8e439b1d1194f92
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2014-01-31 11:11:12 +0100

    Add static root

--- gracedemo/gracedemo/settings.py
+++ gracedemo/gracedemo/settings.py
@@ -1,4 +1,7 @@
 # Django settings for gracedemo project.
+import os
+
+ABSOLUTE_PATH = os.getcwd()
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -60,7 +63,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+#STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(ABSOLUTE_PATH, 'collected_static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
