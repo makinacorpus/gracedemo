eba9141f8644dcc8b43256d6a9a35c86dcc5a70b
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2014-02-13 17:17:44 +0100

    Add mouse position control

--- gracedemo/gracemap/static/gracemap/js/gracemap.js
+++ gracedemo/gracemap/static/gracemap/js/gracemap.js
@@ -5,44 +5,39 @@ var highlight;
 
 $(function() {
     
-    osm = new ol.source.OSM('OSM',{
-                /*tileLoadFunction: function(imageTile, src) {
-                    window.setTimeout(function() {
-                        imageTile.getImage().src = src;
-                    }, 1);
-                },
-                url: 'http://{a-c}.tile.openstreetmap.orgr/{z}/{x}/{y}.png'
-                */
-/*                eventListeners: {
-                    tileloaded: function(evt) {
-                        var ctx = evt.tile.getCanvasContext();
-                        if (ctx) {
-                            var imgd = ctx.getImageData(0, 0, evt.tile.size.w, evt.tile.size.h);
-                            var pix = imgd.data;
-                            for (var i = 0, n = pix.length; i < n; i += 4) {
-                                pix[i] = pix[i + 1] = pix[i + 2] = (3 * pix[i] + 4 * pix[i + 1] + pix[i + 2]) / 8;
-                            }
-                            ctx.putImageData(imgd, 0, 0);
-                            evt.tile.imgDiv.removeAttribute("crossorigin");
-                            evt.tile.imgDiv.src = ctx.canvas.toDataURL();
-                        }
-                    }
-                }*/
-            });
+    osm = new ol.source.OSM('OSM');
     osmLayer = new ol.layer.Tile({source: osm});
+    osmLayer.on('postcompose', function(event) {
+        var context = event.context;
+        var canvas = context.canvas;
+        var image = context.getImageData(0, 0, canvas.width, canvas.height);
+        var data = image.data;
+        for (var i = 0, ii = data.length; i < ii; i += 4) {
+            data[i] = data[i + 1] = data[i + 2] = (3 * data[i] + 4 * data[i + 1] + data[i + 2]) / 8;
+        }
+        context.putImageData(image, 0, 0);
+    });
+
+    var mousePositionControl = new ol.control.MousePosition({
+        coordinateFormat: ol.coordinate.createStringXY(4),
+        projection: 'EPSG:4326',
+        // comment the following two lines to have the mouse position
+        // be placed within the map.
+        className: 'custom-mouse-position',
+        target: document.getElementById('mouse-position'),
+        undefinedHTML: '&nbsp;'
+    });    
     
-    map = new ol.Map({        
+    map = new ol.Map({
+        controls: ol.control.defaults().extend([mousePositionControl]),
         layers: [
             osmLayer
-            /*new ol.layer.Tile({
-                source: osm
-            })*/
         ],
         renderer: 'canvas',
         target: 'map',
         view: new ol.View2D({
-            center: ol.proj.transform([-0.3, 45], 'EPSG:4326', 'EPSG:3857'),
-            zoom: 10
+            center: ol.proj.transform([-0.2385, 44.9313], 'EPSG:4326', 'EPSG:3857'),
+            zoom: 12
         })
     });
 
@@ -132,6 +127,9 @@ function addGeoJSON(table_name) {
         source: objSource,
         styleFunction: styleFunction
     }); 
+    //objSource.on('addfeature', function(event) {
+    //    window.console.log('added feature');
+    //});    
     map.addLayer(json_layer);
     
     id = table_name;
